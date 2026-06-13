7a9f8c78901a03eb4c03e558751dc5d3b1767f11
LvcnC <lvcnncchrc0@outlook.com>
2026-06-13 12:17:10 +0200

    aggiunti file per le statistiche >-<

--- src/main/java/org/openjfx/model/GameEngine.java
+++ src/main/java/org/openjfx/model/GameEngine.java
@@ -88,19 +88,19 @@ public class GameEngine {
         }
     }
 
-    /** humanPlayCard() checks if the card is playable and then execute the move.
-     * @param chosenCard
-     */
     public Card getCurrentCard(){
         return new Card(currentColor, currentCard.getType(), currentCard.getValue());
     }
 
+    /** humanPlayCard() checks if the card is playable and then execute the move.
+     * @param chosenCard
+     */
     public void humanPlayCard(Card chosenCard) {
         Player human = state.getCurrentPlayer();
 
         if (!(human instanceof HumanPlayer)) return;
 
-        if (chosenCard.isPlayableOn(currentCard) || chosenCard.getcolor() == currentColor) {
+        if (chosenCard.isPlayableOn(currentCard) || chosenCard.getColor() == currentColor) {
             state.getMatchStats().decrementPointsTo(human,chosenCard);
       
             executeMove(human, chosenCard);
@@ -329,6 +329,9 @@ public class GameEngine {
             forcedToDraw(challenged,4);
         else
             forcedToDraw(challenger,6);
+
+        state.getMatchStats().incrementChallenges(challenger);
+        state.getMatchStats().incrementChallenges(challenged);
     }
 
     /** forcedToDraw() adds a specified amount of cards to the hand of a specific player
---
+++ src/main/java/org/openjfx/model/PlayerStats.java
@@ -0,0 +1,47 @@
+package org.openjfx.model;
+
+/*
+ *Le statistiche devono includere almeno:
+numero di partite vinte da ciascun giocatore o profilo di bot;
+punteggio medio;
+numero medio di round per partita;
+numero medio di turni per round;
+numero totale di challenge;
+numero totale di penalità applicate.
+ *
+ */
+public class PlayerStats {
+    private int wonMatches = 0;
+    private int overallScore = 0;
+    private int totalMatches = 0;
+    private int numPenalties = 0;
+    private int numChallenges = 0;
+
+    public void registerMatch(boolean hasWon, int winningScore, int numPenalties, int numChallenges){
+        this.totalMatches++;
+        this.numPenalties += numPenalties;
+        this.numChallenges += numChallenges;
+
+        if (hasWon) {
+            wonMatches++;
+            updateScore(winningScore);
+        }
+
+    }
+
+    public int getOverallScore() {
+        return this.overallScore;
+    }
+    public void setOverallScore(int nMatchScore){
+        this.overallScore = nMatchScore;
+    }
+
+    public void updateScore(int nScore){
+        if (nScore > 0) this.overallScore += nScore;
+    }
+
+    public int getTotalWonMatches(){
+        return wonMatches;
+    }
+
+}
