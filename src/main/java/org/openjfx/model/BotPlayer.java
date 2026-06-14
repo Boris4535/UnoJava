fee6e333339e03996b2d675ff9949861a283ac1c
leon44510 <emanuele.balbo45@gmail.com>
2026-06-14 22:38:04 +0200

    Hotfix:  previous version could lead to a bot not being able to play any card

--- src/main/java/org/openjfx/model/BotPlayer.java
+++ src/main/java/org/openjfx/model/BotPlayer.java
@@ -15,7 +15,7 @@ public class BotPlayer extends Player {
     public Card BotPlays(Card current){
         List<Card> playable = getHand().stream().filter(c -> c.isPlayableOn(current)).toList();
         if(playable.isEmpty()) return null;
-        if(playable.size() == 1) return playable.get(0);
+        if(playable.size() == 1) return playable.getFirst();
         return switch(personality){
             case STUPID -> StupidPlay(playable);
             case CLEVER -> CleverPlay(playable);
@@ -50,11 +50,13 @@ public class BotPlayer extends Player {
         else{
             for(Card c: hand)
                 if(c.getColor() != current.getColor() &&
-                        (currentType == CardType.NUMBERS) && c.getType() == CardType.NUMBERS) return c;
+                        (currentType == CardType.NUMBERS && c.getType() == CardType.NUMBERS) &&
+                        current.getValue() == c.getValue()) return c;
         }
         //If all else fails, pick a numbered card
-        return hand.get(types.indexOf(1));
-
+        if(types.contains(1)) return hand.get(types.indexOf(1));
+        // If that  doesn't work either
+        else return hand.getFirst();
     }
 
     //Picks a card based on a priority list which can later be altered if needed
